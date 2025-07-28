04c3bd387484078bd794d061c9cab9a4c92f9d16
Matheus Carvalho <matheusth26t@gmail.com>
2025-07-28 10:03:49 -0300

    feat: setup conexão com o banco de dados

--- src/app.js
+++ src/app.js
@@ -1,4 +1,13 @@
 import express from 'express';
+import connectToDatabase from './config/db.js';
+
+const db = await connectToDatabase();
+db.on('error', () => {
+  console.error('Database connection error');
+});
+db.once('open', () => {
+  console.log('Database connected successfully');
+});
 
 const app = express();
 app.use(express.json());
